d22626c41cf04c9c69af781688e4a7d0ec2d708d
Harsh <raghavharsh88@gmail.com>
2023-03-19 11:24:00 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        // rwOnly.name = "harsh";
+        // System.out.println(rwOnly.name);
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        rwOnly.setName("harsh");
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
